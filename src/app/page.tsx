f165c1ca477cc4e899a0fb7b7e304beada0c2c5e
helloyanis <helloyanis@hotmail.com>
2025-02-07 17:16:48 +0100

    Add some text to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 "use client"
+import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+import { Box, CircularProgress, Typography } from '@mui/material';
+
 export default function Home() {
+  const { t, i18n } = useTranslation();
+  const [isReady, setIsReady] = useState(false);
 
+  useEffect(() => {
+    if (i18n.isInitialized) {
+      setIsReady(true);
+    }
+  }, [i18n.isInitialized]);
   return (
-    <div className="w-100 min-h-screen py-2">
-      Hello world
-
-    </div>
+    <Box className="max-w-[900px] m-auto" sx={{ display: 'flex', justifyContent: 'center', height: 'calc(100vh - 64px)' }}>
+      {isReady ? (
+        //Page content
+        <Box>
+          <Typography variant='h2' className='font-bold m-3 text-center'>{t('title')}</Typography>
+          <Typography variant='subtitle1'>{t('content1')}</Typography>
+        </Box>
+      ) : (
+        //Loading spinner while translations load
+        <CircularProgress className='m-auto' />
+      )
+      }
+    </Box>
   );
 }
